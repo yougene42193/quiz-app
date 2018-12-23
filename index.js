3931939d5e10c508d280eb599d1151ff190c10ee
Eugene Song <yougene42193@gmail.com>
2018-12-22 20:19:23 -0500

    added wireframe for html

---
+++ index.js
@@ -0,0 +1,44 @@
+'use strict';
+const STORE = [
+  {
+    question: 'Derrick Rose was the youngest player to win MVP. How old was he when he won the MVP?',
+    answers: [
+      '24 years old',
+      '22 years old',
+      '19 years old',
+      '21 years old'
+      ],
+    rightAnswer: '22 years old',
+  },
+  {
+    question: 'Which team holds the record for most consecutive missed 3 pointers in a playoff game?',
+    answers: [
+      'New York Knicks',
+      'Oklahoma City Thunder',
+      'Cleveland Cavaliers',
+      'Houston Rockets',
+    ],
+    rightAnswer: 'Houston Rockets',
+  },
+  {
+    question: 'Which player did not win the Rookie of the Year award?',
+    answers: [
+      'James Harden',
+      'Emeka Okafor',
+      'Kevin Durant',
+      'Tyreke Evans',
+    ],
+    rightAnswer: 'James Harden',
+  },
+  {
+    question: 'Which players has never score more then 50 points in a game?',
+    answers: [
+      'Corey Brewer',
+      'Paul George',
+      'Terrence Ross',
+      'Anthony Davis',
+    ],
+    rightAnswer: 'Paul George',
+  },
+  {}
+];
